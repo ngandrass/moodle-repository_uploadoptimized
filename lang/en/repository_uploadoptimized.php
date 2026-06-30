ef6fb8eaadad5db35de7899405e68c7118053b5e
Niels Gandraß <niels@gandrass.de>
2026-06-30 13:25:04 +0200

    Add first working prototype

---
+++ lang/en/repository_uploadoptimized.php
@@ -0,0 +1,41 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_uploadoptimized', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   repository_uploadoptimized
+ * @copyright 2026 Niels Gandraß <niels@gandrass.de
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configplugin'] = 'Configuration for optimized upload plugin';
+$string['pluginname_help'] = 'Upload and optimize a file to Moodle';
+$string['pluginname'] = 'Upload and optimize a file';
+$string['upload:view'] = 'Use uploading and optmmize in file picker';
+$string['upload_error_ini_size'] = 'The file is larger than the maximum size allowed.';
+$string['upload_error_form_size'] = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
+$string['upload_error_partial'] = 'The uploaded file was only partially uploaded.';
+$string['upload_error_no_file'] = 'No file was uploaded.';
+$string['upload_error_no_tmp_dir'] = 'PHP is missing a temporary folder.';
+$string['upload_error_cant_write'] = 'Failed to write file to disk.';
+$string['upload_error_extension'] = 'A PHP extension stopped the file upload.';
+$string['upload_error_folders_not_supported'] = 'Compress the folder into a ZIP file to upload it.';
+$string['upload_error_folders_not_supported_title'] = 'This folder can\'t be uploaded';
+$string['upload_error_invalid_file'] = 'The file \'{$a}\' is either empty or a folder. To upload folders zip them first.';
+$string['privacy:metadata'] = 'The upload and optimize a file repository plugin does not store or transmit any personal data.';
